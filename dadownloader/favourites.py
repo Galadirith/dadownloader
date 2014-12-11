c686c9e11f3b276f8824bae29788b632090a67d3
Edward Fauchon-Jones <edward.fauchon.jones@gmail.com>
2014-12-11 23:56:52 +0000

    Add Favourites class
    Tested with `dadl velinov` which at the time of commit had 2
    sub-collections st the output correctly revealed 3 collections,
    'Favourites', 'Photography' and 'artistic nude'.

---
+++ dadownloader/favourites.py
@@ -0,0 +1,60 @@
+from    StringIO    import StringIO
+from    lxml        import etree
+
+class Favourites:
+    """
+    Object to hold a complete set of favourite collections
+
+    This Object encapsulates a complete set of collections from a DeviantArt
+    users favourites. This class also contains the crucial methods that actually
+    gather all of the data from deviantART :D
+
+    :var str url: URL to the deviants user page to whom the favourites belong.
+    :var str favurl: URL to the favourites page.
+    :var dadownloader.collection[] collections: List of collections including
+        the root collection of the users favourites.
+    :var requests.Session session: An instance through which all remote requests
+        should be made.
+    """
+
+    def __init__(self, username, session):
+        """
+        :param str username: Username of the deviant from whom we wish to
+            download the favourites from.
+        :param requests.Session session: An instance through which all remote
+            requests should be made.
+        """
+        self.url            = 'http://%s.deviantart.com' % username
+        self.favurl         = self.url+'/favourites/'
+        self.session        = session
+        self.collections    = []
+
+        self.grabCols()
+
+    def pushCol(self, name, url):
+        """
+        Adds a single favourite collection to the container
+
+        :param str name: Name of the collection
+        :param str url: URL to the collection
+        """
+        self.collections.append({'name': name, 'url': url})
+
+    def grabCols(self):
+        """Adds every favourite collection to the container"""
+
+        # Add the root collection
+        self.pushCol('Favourites', self.favurl)
+
+        # Load the deviants root collection page
+        root = self.session.get(self.favurl)
+
+        # Harvest the name and url of each sub-collection
+        parser      = etree.HTMLParser()
+        rootXML     = etree.parse(StringIO(root.text), parser)
+        colNames    = rootXML.xpath('//div[@class="tv150"]/div[@class="tv150-tag"]/text()')
+        colURLs     = rootXML.xpath('//div[@class="tv150"]/a[@class="tv150-cover"]/@href')
+
+        # Push each sub-collection to collections
+        for i in range(len(colNames)):
+            self.pushCol(colNames[i], colURLs[i])
