627f3d6e7330f0d953ab4e37d2380b4881316be6
Edward Fauchon-Jones <edward.fauchon.jones@gmail.com>
2014-12-15 13:15:35 +0000

    Refactor Auth's high-level authentication logic
    Auth.auth() has been refactored st its now much simpler to follow the
    authentication pipeline. Tasks such as verifying a session is
    authenticated againsts DeviantArt have been spun off into separate
    methods.
    
    Log in credentials are also now saved to file. This creates a simpler
    user experience in the case when cookies become invalid (which for
    requests seems to be sooner than their expiration date in my experience.
    I have yet to determine why this is) st a user should not have to
    re-enter login details unless they have updated them on DeviantArt.
    
    Note that it is still preferable to use existing authenticated cookies
    as I am unsure of what the DA servers would think if you made many log
    in attempts, valid or not, over a short space of time. I am assuming of
    course the extreme case where the program is run many times in quick
    succession, which is actually unlikely :D

--- dadownloader/auth.py
+++ dadownloader/auth.py
@@ -15,109 +15,83 @@ class Auth:
     """
 
     def __init__(self):
-        self.url        = 'https://www.deviantart.com/users/login'
-        self.session    = self.createSession()
+        self.url = 'https://www.deviantart.com/users/login'
+        self.newSession()
         pass
 
     def auth(self):
         """
         Attempt to generate an authenticated session for DeviantArt.
 
-        Check for a 'cookies' file in the calling directory. If no such file
-        exists prompt the user for DeviantArt login credentials. An
-        authenticated session is required to access ristricted content on
+        An authenticated session is required to access restricted content on
         DeviantArt, such as mature deviations, and those flaged by their creator
         to only allow access from DeviantArt members.
 
         :rtype: requests.Session
         :return:
-            If a 'cookies' file exists in the calling directory, they are
-            assumed to be a pickle dump of a valid cookiejar. Deterimine if they
-            are authenticated cookies for DeviantArt. If they are not request
-            login credentials.
-
-            If valid credentials are provided, the returned object contains
-            authenticated cookies.
-
-            If no or invalid credentials are provided, the returned object will
-            contain no authenticated cookies.
+            If Auth was able to log in to DeviantArt, a session object is
+            returned containing cookies authenticated against DeviantArt,
+            otherwise an unauthenticated seesion object is returned.
         """
-        url     = self.url
-        session = self.session
-
         # Check for authenticated cookies
         if os.path.isfile('cookies'):
-
             with open('cookies', 'rb') as f:
-                session.cookies = pickle.load(f)
+                self.session.cookies = pickle.load(f)
 
+            # Check if they are authenticated against DeviantArt
+            print('Checking cookies')
             status = self.verify()
             if status == 'GOOD':
                 print('Cookies are good')
-                return session
+                return self.session
             elif status == 'BAD':
+                # Delete the bad cookies
+                os.remove('cookies')
                 print('Cookies are bad')
 
-        # Start a new/clean session
-        session = self.createSession()
+        # Check for saved login credentials
+        if os.path.isfile('credentials'):
+            with open('credentials', 'r') as f:
+                username = f.readline().strip()
+                password = f.readline().strip()
 
-        # Reqeust login credentials from the user
+            # Attempt to log in to DeviantArt
+            print('Checking log in details')
+            status = self.login(username, password)
+            if status == 'GOOD':
+                print('Log in details are good')
+                return self.session
+            elif status == 'BAD':
+                # Delete the bad credentials
+                os.remove('credentials')
+                print('Log in details are bad')
+
+        # If there are no saved details reqeust login credentials from the user
         print(\
             'If you want to access restricted content '\
-            'please provide login credentials.')
+            'please provide login detail.')
         username = raw_input('Username: ')
         password = getpass.getpass()
 
-        if username == '' or password == '':
-            print(\
-                'Your username or password is empty. '\
-                'Continuing unauthenticated.')
-            return session
+        # Attempt to log in to DeviantArt
+        print('Checking log in details')
+        status = self.login(username, password)
+        if status == 'GOOD':
+            print('Log in details are good')
+            return self.session
+        elif status == 'BAD':
+            print('Log in details are bad. Continuing unauthenticated.')
 
-        # Request the DeviantArt login page
-        response = session.get(url, headers={'Referer': url})
+        return self.session
 
-        # Find cross-site scripting (XSS) variables requried for POST payload
-        parser  = etree.HTMLParser()
-        doc     = etree.parse(StringIO(response.text), parser)
-        token   = doc.xpath('//*[@id="login"]/input[2]/@value')[0]
-        key     = doc.xpath('//*[@id="login"]/input[3]/@value')[0]
-
-        # Simulate an application/x-www-form-urlencoded POST request to login
-        payload = {
-            'ref': url,
-            'username': username,
-            'password': password,
-            'remember_me': '1',
-            'validate_token': token,
-            'validate_key': key
-        }
-        session.post(url, data=payload, headers={'Referer': url})
-
-        # Write cookies to file for future re-use
-        with open('cookies', 'w+b') as f:
-            pickle.dump(session.cookies, f)
-
-        # Assign the new session to the instance field self.session
-        self.session = session
-
-        # Check for authenticated cookies
-        status = self.verify()
-        if status == 'BAD':
-            print('Cookies are bad. Continuing unauthenticated.')
-
-        return session
-
-    def createSession(self):
+    def newSession(self):
         """Create a new session for remote requests to DeviantArt"""
 
         userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) '\
                     'AppleWebKit/537.36 (KHTML, like Gecko) '\
                     'Chrome/39.0.2171.71 Safari/537.36'
-        session                         = requests.Session()
-        session.headers['User-Agent']   = userAgent
-
-        return session
+        self.session                        = requests.Session()
+        self.session.headers['User-Agent']  = userAgent
 
     def verify(self):
         """
@@ -138,3 +112,66 @@ class Auth:
             return 'GOOD'
         else:
             return 'BAD'
+
+    def login(self, username, password):
+        """
+        Attempt to log in the current session to DeviantArt
+
+        :param str username: DeviantArt username.
+        :param str password: DeviantARt password.
+        :rtype: str
+        :return: If the user is able to log in to DeviantArt return 'GOOD',
+            otherwise return 'BAD'.
+        """
+        username = username.strip()
+        password = password.strip()
+
+        # Create new/clean session
+        self.newSession()
+
+        # Check if credentials are empty
+        if username == '':
+            print(\
+                'Your username is empty. '\
+                'Continuing unauthenticated.')
+            return session
+        elif password == '':
+            print(\
+                'Your password is empty. '\
+                'Continuing unauthenticated.')
+            return self.session
+
+        # Request the DeviantArt login page
+        response = self.session.get(self.url, headers={'Referer': self.url})
+
+        # Find cross-site scripting (XSS) variables requried for POST payload
+        parser  = etree.HTMLParser()
+        doc     = etree.parse(StringIO(response.text), parser)
+        token   = doc.xpath('//*[@id="login"]/input[2]/@value')[0]
+        key     = doc.xpath('//*[@id="login"]/input[3]/@value')[0]
+
+        # Simulate an application/x-www-form-urlencoded POST request to login
+        payload = {
+            'ref': self.url,
+            'username': username,
+            'password': password,
+            'remember_me': '1',
+            'validate_token': token,
+            'validate_key': key
+        }
+        self.session.post(self.url, data=payload, headers={'Referer': self.url})
+
+        # Check log in details
+        status = self.verify()
+        if status == 'GOOD':
+            # Write cookies to file for future re-use
+            with open('cookies', 'w+b') as f:
+                pickle.dump(self.session.cookies, f)
+            # Write credentials to file for future re-use
+            with open('credentials', 'w+b') as f:
+                f.write(username)
+                f.write('\n')
+                f.write(password)
+                f.write('\n')
+
+        return status
